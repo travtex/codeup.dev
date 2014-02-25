3cb557c47851796acd79ce2b28f910b9b11ca053
travtex <tflatt74@gmail.com>
2014-02-25 09:16:49 -0600

    added jquery ui

--- public/todo-list.php
+++ public/todo-list.php
@@ -73,7 +73,16 @@
 
 	</style>
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+	<script>
+	  $(function() {
+	    $( "#sortable" ).sortable();
+	    $( "#sortable" ).disableSelection();
+	  });
+  </script>
 </head>
 <body>
 
@@ -96,7 +105,7 @@
 			exit;
 		}
 	?>
-		<ul>
+		<ul id="sortable">
 			<?php 
 
 			if($items){
