0f434bf5394d08ece54b727daf858b9fef49c072
travtex <tflatt74@gmail.com>
2014-02-26 15:50:22 -0600

    working prototype address_book.php

--- public/address_book.php
+++ public/address_book.php
@@ -55,6 +55,13 @@ if(!empty($_POST)){
 	}
 }
 
+if(isset($_GET['remove'])){
+	unset($address_array[$_GET['remove']]);
+	save_addresses($filename, $address_array);
+	header("Location: address_book.php");
+	exit(0);
+}
+
 ?>
 
 
@@ -68,8 +75,48 @@ if(!empty($_POST)){
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+    <style type="text/css">
+    html {
+        height: 100%;
+    }
+    body {
+        background-image: url('img/dark-blue-background.jpg');
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-attachment: fixed;
+        margin: 20px;
+    }
+    hr {
+        height: 2px;
+        border-color: teal !important;
+        border-width: 2px !important;
+    }
+    div {
+        padding: 20px;
+        border-radius: 20px;
+
+    }
+
+    #main {
+        background: -webkit-radial-gradient(left top, lightsteelblue, white);
+        margin: 20px auto;
+        width: 85%;
+        border-style:groove;
+        border-width: 3px;
+        border-color: teal;
+    }
+
+    #main h2{
+        color: darkgreen;
+    }
+
+    ul {
+        font-size: 14pt;
+    }
+    </style>
 </head>
 <body>
+	<div id="main">
 	<h2>Address Book</h2>
 	<table class="table">
 		<tr>
@@ -79,13 +126,15 @@ if(!empty($_POST)){
 			<td>State</td>
 			<td>Zip</td>
 			<td>Phone</td>
+			<td>Delete?</td>
 		</tr>
 		<? if(!empty($address_array)): ?>
-			<? foreach($address_array as $address): ?>
+			<? foreach($address_array as $key => $address): ?>
 			<tr>
 				<? foreach($address as $value): ?>
 				<td><?= $value; ?></td>
 				<? endforeach; ?>
+				<td><a href="?remove=<?= $key ?>">X</a></td>
 			</tr>
 			<? endforeach; ?>
 		<? endif; ?>
@@ -119,6 +168,7 @@ if(!empty($_POST)){
 		    			<? endforeach; ?></mark></p>
     		<? endif; ?>
         	<button type="submit" class="btn btn-primary">Add New Address</button>
+        </div>
         </div>
     </form>
 
