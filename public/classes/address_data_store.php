73fabeee7e29d758647b0a01748a70232eb8fb05
travtex <tflatt74@gmail.com>
2014-02-28 11:32:16 -0600

    refactoring address_data_store.php

--- public/classes/address_data_store.php
+++ public/classes/address_data_store.php
@@ -5,14 +5,11 @@ require_once('filestore.php');
 class AddressDataStore extends Filestore {
 
 	function set_entry($array) {
-		$entry = [
-			htmlspecialchars(strip_tags($array['name'])),
-			htmlspecialchars(strip_tags($array['address'])),
-			htmlspecialchars(strip_tags($array['city'])),
-			htmlspecialchars(strip_tags($array['state'])),
-			htmlspecialchars(strip_tags($array['zip'])),
-			htmlspecialchars(strip_tags($array['phone']))
-			];
+		$entry = [];
+
+		foreach($array as $item) {
+			$entry[] = htmlspecialchars(strip_tags($item));
+		}
 		return $entry;
 	}
 }
