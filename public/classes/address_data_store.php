7ab163d85a9d325bacf2b03c301eaff17740222c
travtex <tflatt74@gmail.com>
2014-03-03 09:17:45 -0600

    parent exercise

--- public/classes/address_data_store.php
+++ public/classes/address_data_store.php
@@ -4,7 +4,12 @@ require_once('filestore.php');
 
 class AddressDataStore extends Filestore {
 
-	function set_entry($array) {
+	public function __construct($filename= '') {
+		parent::__construct($filename);
+		$this->$filename = strtolower($filename);
+	}
+
+	public function set_entry($array) {
 		$entry = [];
 		foreach($array as $item) {
 			$entry[] = htmlspecialchars(strip_tags($item));
