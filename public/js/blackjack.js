3f0b0b235c5a8f043fa3dfac906dbe9850357cb7
travtex <tflatt74@gmail.com>
2014-03-05 11:24:15 -0600

    updated dates to remove odd forEach decision

---
+++ public/js/blackjack.js
@@ -0,0 +1,12 @@
+var suits = ['Harts', 'Wolves', 'Kraken', 'Lions'];
+var values = ['Ace', 'King', 'Queen', 'Jack', 'Ten', 'Nine', 'Eight', 'Seven', 'Six',
+				'Five', 'Four', 'Three', 'Two'];
+
+
+console.log(suits.length);
+function buildDeck(suits, values) {
+	for(var i = 1; i < suits.length; i++) {
+
+	}
+}
+
