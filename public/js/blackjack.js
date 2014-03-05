71e157dfc97ab70e7ac7b9b46db35f6c8d4f7f2e
travtex <tflatt74@gmail.com>
2014-03-05 11:43:17 -0600

    starting on js blackjack

--- public/js/blackjack.js
+++ public/js/blackjack.js
@@ -2,11 +2,17 @@ var suits = ['Harts', 'Wolves', 'Kraken', 'Lions'];
 var values = ['Ace', 'King', 'Queen', 'Jack', 'Ten', 'Nine', 'Eight', 'Seven', 'Six',
 				'Five', 'Four', 'Three', 'Two'];
 
+var deck = [];
 
-console.log(suits.length);
 function buildDeck(suits, values) {
-	for(var i = 1; i < suits.length; i++) {
+	for(var i = 0; i < values.length; i++) {
+		for(var j = 0; j < suits.length; j++) {
+			deck.push(values[i] + " of " + suits[j]);
 
+		}
 	}
 }
+
+buildDeck(suits, values);
+console.log(deck.length);
 
